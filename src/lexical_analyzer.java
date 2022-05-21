261adadf47da401a957cc98ce56daebf3689336e
Cristopher Bohol <cris.bacus6@gmail.com>
2022-05-21 13:59:29 +0800

    lexical

--- src/lexical_analyzer.java
+++ src/lexical_analyzer.java
@@ -1,3 +1,8 @@
 public class lexical_analyzer {
-    
+    public static String[] lex_analyze(String lexemes ){
+        String[] lexemes_array = lexemes.split("\\s+");
+        return lexemes_array;
+    }
+
+    // 
 }
